2d1a85d8fe15e9d6f1d005ff7fbda5009a5752ae
CanulTamayAlex03 <124325990+CanulTamayAlex03@users.noreply.github.com>
2024-08-19 17:08:50 -0600

    cambios

--- QRCode.js
+++ QRCode.js
@@ -1,31 +1,44 @@
 function generateQRCode() {
     const url = document.getElementById("urlInput").value;
     const qrCodeContainer = document.getElementById("qrCode");
-    qrCodeContainer.innerHTML = ""; 
+    const downloadLink = document.getElementById("downloadLink");
+    qrCodeContainer.innerHTML = "";
+    downloadLink.style.display = "none";
 
     if (url) {
-        // Crear el QR code con la imagen
         const qr = new QRious({
-            element: document.createElement('canvas'), 
-            value: url, 
-            size: 190, 
+            element: document.createElement('canvas'),
+            value: url,
+            size: 190,
             level: 'H',
-            foreground: '#105E85',
-            background: '#FFFFFF' 
+            foreground: '#1E1F6C',
+            background: '#FFFFFF'
         });
 
-        // Agregar el logo encima del QR
+        qrCodeContainer.appendChild(qr.canvas);
+
         const ctx = qr.canvas.getContext('2d');
         const img = new Image();
-        img.src = 'test.png'; // Usa la imgagen que desees (local o en línea)
+        img.crossOrigin = "anonymous"; // Habilitar CORS para recursos externos
+        img.src = 'https://images.squarespace-cdn.com/content/v1/62a42c7be2e856044c6d9bde/1658211947435-HE66YCONE25811RCPQJQ/clipboard.png'; // Usa una URL pública
+
         img.onload = function() {
-            const x = (qr.size - 50) / 2;
-            const y = (qr.size - 50) / 2;
-            ctx.drawImage(img, x, y, 50, 50); 
-            qrCodeContainer.appendChild(qr.canvas); 
+            const x = (qr.size - 55) / 2;
+            const y = (qr.size - 55) / 2;
+            ctx.drawImage(img, x, y, 50, 50);
+
+            // Crear el enlace de descarga
+            qr.canvas.toBlob(function(blob) {
+                const url = URL.createObjectURL(blob);
+                downloadLink.href = url;
+                downloadLink.style.display = "inline-block";
+            }, "image/png");
+        };
+
+        img.onerror = function() {
+            console.error("Error al cargar la imagen.");
         };
     } else {
         alert("Por favor, introduce un enlace válido.");
     }
 }
-
